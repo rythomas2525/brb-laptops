08708034dab68f4e7bdc2d8fce08055b23e9ce6e
aarkitekkt <johnson.brycej@gmail.com>
2020-03-04 21:34:08 -0700

    built laptop model

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO laptops (brand, model, operating_system, laptop_condition, processor, graphics, ram, screen_dimension, hd_storage, release_year, summary, price)
+VALUES ("Apple","MacBook Pro","Mac OS",5,"i9","AMD Radeon Pro 5500M with 8GB of GDDR6",64,16,"8TB",2020,"Comes with touch bar and touch id",6000),
+("Dell","Alienware Area-51M","Windows 10",5,"i9","NVIDIA GeForce RTX™ 2080 8GB GDDR6",64,17,"2TB",2020,"CRYO-TECH COOLING, Whisper-quiet",4295),
+("ASUS","Tuf","Windows 10",4,"Quad-core AMD Ryzen 7 R7-3750H Processor","NVIDIA GeForce RTX 2060 8GB GDDR6",16,15,"512GB",2019,"RGB Backlit keyboard rated for 20-million keystroke durability,Dual fans with anti-dust technology",1099.99),
+("HP","Omen","Windows 10",5,"Intel Hexa-Core i7","NVIDIA GeForce GTX 1070",16,17,"1TB",2019,"Backlit Keyboard, B&O dual speakers, HP Audio Boost, DTS Headphone:X support",1,439);
