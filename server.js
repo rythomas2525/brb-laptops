6a2cc87009c871179b8dda908c4f6f7ddf1a369f
Ryan Thomas <rythomas2525@gmail.com>
2020-03-05 19:01:24 -0700

    added handlebars to server

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Routes
 // =============================================================
 require("./routes/api-routes.js")(app);
