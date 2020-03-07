614d985704bbd80718ebe53f9f7cb72a005b9679
aarkitekkt <johnson.brycej@gmail.com>
2020-03-07 11:22:41 -0700

    pull data from form

---
+++ public/js/sell.js
@@ -0,0 +1,23 @@
+$(function () {
+    $("#newLaptop").on("submit", function (event) {
+
+        event.preventDefault();
+
+        var newLaptop = {
+            name: $("#name").val().trim(),
+            email: $("#email").val().trim(),
+            phone: $("#number").val().trim(),
+            city: $("#city").val().trim(),
+            brand: $("#brand").val().trim(),
+            model: $("#model").val().trim(),
+            operating_system: $("#os").val().trim(),
+            laptop_condition: $("#condition").val().trim(),
+            processor: $("processor").val().trim(),
+            graphics: $("#graphics").val().trim(),
+
+        }
+
+        console.log(newLaptop);
+
+    });
+})
