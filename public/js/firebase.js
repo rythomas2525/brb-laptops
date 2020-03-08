b2231c3bcdcd0fc09a2b1355e547ba490eb64565
Ryan Thomas <rythomas2525@gmail.com>
2020-03-08 17:18:59 -0600

    main image button uploads

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,23 +1,51 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyByHflVSt-95YP4yWECkiK6vLGTguS7KFU",
+    authDomain: "future-force-270416.firebaseapp.com",
+    databaseURL: "https://future-force-270416.firebaseio.com",
+    projectId: "future-force-270416",
+    storageBucket: "future-force-270416.appspot.com",
+    messagingSenderId: "397427626203",
+    appId: "1:397427626203:web:e353a58a74c91413f6bcfe",
+    measurementId: "G-H126PY4SSJ"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 
 
 
-const storageService = firebase.storage();
-const storageRef = storageService.ref();
-
-document.querySelector('.file-submit-main').addEventListener('submit', handleFileUploadSubmit);
+var mainPictureButton = document.getElementById("mainPictureButton");
+var uploader = document.getElementById("uploader");
 
 
-$("#newLaptop").on("submit", function (event) {
+mainPictureButton.addEventListener("change", function (e) {
 
-    event.preventDefault();
-
-    var file_data = $(".file-select-main").prop('files')[0]
-    storageRef.put(file_data);
-
-    console.log(newLaptop);
+    console.log(e.target.files[0].name)
+    // get file
+    var file = e.target.files[0];
 
 
+    // create a storage ref
+    var storageRef = firebase.storage().ref("images/" + file.name);
+
+    //  upload file
+    var task = storageRef.put(file);
+
+    // update progress bar
+    task.on('state_changed',
+        function progress(snapshot) {
+            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            uploader.value = percentage;
+        },
+
+        function error(err) {
+
+        },
+
+        function complete() {
 
 
+        })
 
+})
