418d492980ed127a99cb79c08a38bb0b091f05f2
Ryan Thomas <rythomas2525@gmail.com>
2020-03-08 18:36:25 -0600

    added firebase to its own js file to make things more organized

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -8,15 +8,20 @@ var firebaseConfig = {
     appId: "1:397427626203:web:e353a58a74c91413f6bcfe",
     measurementId: "G-H126PY4SSJ"
 };
-
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 
 
-
 var mainPictureButton = document.getElementById("mainPictureButton");
+var secondaryPictureButton = document.getElementById("secondaryPictureButton");
 var uploader = document.getElementById("uploader");
+var secondaryUploader = document.getElementById("secondaryUploader")
+
+
+
+
 
 
 mainPictureButton.addEventListener("change", function (e) {
@@ -44,8 +49,44 @@ mainPictureButton.addEventListener("change", function (e) {
         },
 
         function complete() {
-
+            var URL = task.snapshot.ref.getDownloadURL().then(function (downloadURL) { console.log(downloadURL) })
+            return URL;
 
         })
+
+})
+
+secondaryPictureButton.addEventListener("change", function (e) {
+
+    console.log(e.target.files[0].name)
+    // get file
+    var file = e.target.files[0];
+
+
+    // create a storage ref
+    var storageRef = firebase.storage().ref("secondaryimages/" + file.name);
+
+    //  upload file
+    var task = storageRef.put(file);
+
+    // update progress bar
+    task.on('state_changed',
+        function progress(snapshot) {
+            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            secondaryUploader.value = percentage;
+        },
+
+        function error(err) {
+
+        },
+
+        function complete() {
+
+            var secondaryURL = task.snapshot.ref.getDownloadURL().then(function (downloadURL) { console.log(downloadURL) })
+            return secondaryURL;
+
+        })
+
+
 
 })
