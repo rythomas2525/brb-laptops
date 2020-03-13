8092c51b38b951aeec6b472fa856357232c105e3
Ryan Thomas <rythomas2525@gmail.com>
2020-03-12 20:37:36 -0600

    more bullshit

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ require("./routes/handlebars-routes")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync({}).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
     app.listen(PORT, function () {
         console.log("Server listening on: http://localhost:" + PORT);
     });
